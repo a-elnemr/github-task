7538398e84ac2622069c90967712f269db6e4ed9
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-20 06:54:18 +0200

    Passed Repos to Repos List

---
+++ react-native-app/src/components/ReposList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "react-native";
+
+const RepoCard = ({repo}) =>{
+    console.log(repo)
+    return <></>
+}
+
+const ReposList = ( {repos} ) =>
+{
+    console.log(repos);
+    return <Text>R</Text>;
+}
+
+export default ReposList;
--- react-native-app/src/routes/Explore.js
+++ react-native-app/src/routes/Explore.js
@@ -1,13 +1,21 @@
 import { Text } from "react-native";
 import { useGetGithubByNameQuery } from "../services/github";
+import ReposList from "../components/ReposList";
+
 
 
 const Explore = () => {
     const { data, error, isLoading } = useGetGithubByNameQuery()
     console.log(data, error, isLoading);
-    
+    const toRender = error ? (
+            <Text>Oh no, there was an error</Text>
+        ) : isLoading ? (
+            <Text>Loading...</Text>
+        ) : data ? (
+            <ReposList repos={data.items}/>
+        ) : null;
 
-    return (<Text>Explore</Text>)
+    return toRender;
 };
 
 export default Explore;
