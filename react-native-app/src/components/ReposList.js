76c9c81b0f008ff1d06568b051a0251243b28b20
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-20 10:15:16 +0200

    Added Human Readable time

--- react-native-app/src/components/ReposList.js
+++ react-native-app/src/components/ReposList.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 import { List } from 'react-native-paper';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import moment from 'moment';
 
 
 
@@ -16,8 +17,10 @@ const RepoCard = ({repo}) => {
   
   //console.log(JSON.stringify(repo));
 
-  console.log(Date.parse(repo.updated_at));
-
+  //console.log(Date.parse(repo.updated_at));
+  const diff = Date.now()-Date.parse(repo.updated_at);
+  var duration = moment.duration(diff);
+  const time_difference = "Updated "+duration.humanize() + " ago";
 
   return (<Card style={{margin:4}}>
     <Card.Title title={repo.full_name} left={LeftContent} />
@@ -25,6 +28,9 @@ const RepoCard = ({repo}) => {
       <Title>Card title</Title>
       <Paragraph>{repo.description}</Paragraph>
       <Paragraph>{repo.description}</Paragraph>
+      <Paragraph>{time_difference}</Paragraph>
+      <Paragraph>{time_difference}</Paragraph>
+      
     </Card.Content>
     <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
     <Card.Actions>
