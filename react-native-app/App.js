6fe8c21d9186425f799e0ab16d19c3a0471fec01
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-20 05:54:35 +0200

    App provided with Redux store

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,26 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Router, Route, Link } from "./react-router";
+import { Provider } from 'react-redux';
+import { store } from "./src/app/store";
 
 const Home = () => <Text>Home</Text>;
 
 const About = () => <Text>About</Text>;
 
 const App = () => (
-  <Router>
-    <View style={styles.container}>
-      <View style={styles.nav}>
-        <Link to="/">
-          <Text>Home</Text>
-        </Link>
-        <Link to="/about">
-          <Text>About</Text>
-        </Link>
-      </View>
+  <Provider store={store}>
+    <Router>
+      <View style={styles.container}>
+        <View style={styles.nav}>
+          <Link to="/">
+            <Text>Home</Text>
+          </Link>
+          <Link to="/about">
+            <Text>About</Text>
+          </Link>
+        </View>
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </View>
-  </Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </View>
+    </Router>
+  </Provider>
 );
 
 const styles = StyleSheet.create({
