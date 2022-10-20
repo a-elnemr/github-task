b375cc2d3220cad7fb3d691584005a0bb3d7b8df
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-20 11:07:16 +0200

    Added book icon

---
+++ react-native-app/src/components/Icons.js
@@ -0,0 +1,8 @@
+import Icon from 'react-native-vector-icons/Foundation';
+
+const FoundationIcon = Icon;
+
+
+export default FoundationIcon;
+
+
--- react-native-app/src/components/ReposList.js
+++ react-native-app/src/components/ReposList.js
@@ -4,40 +4,39 @@ import { List } from 'react-native-paper';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import FoundationIcon from "./Icons";
 
 
 
 
 
-
-
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+const LeftContent = props => <Avatar.Icon {...props} icon="book-variant" style={{backgroundColor:"transparent"}} color="cyan"/>;
 
 const RepoCard = ({repo}) => {
 
+const BookIcon =(props)=> <FoundationIcon {...props} name="book-bookmark" size={30} color="#00ee00"/>;
   
-  //console.log(JSON.stringify(repo));
-
-  //console.log(Date.parse(repo.updated_at));
   const diff = Date.now()-Date.parse(repo.updated_at);
   var duration = moment.duration(diff);
   const time_difference = "Updated "+duration.humanize() + " ago";
-  const startIcon = <Icon name="star-o" size={15} color="#00ee00" />;;
+  const starIcon = <Icon name="star-o" size={15} color="#00ee00" />;
 
-  return (<Card style={{margin:4}}>
-    <Card.Title title={repo.full_name} left={LeftContent} />
-    <Card.Content>
-      <Paragraph>{repo.description}</Paragraph>
-      <Paragraph>{time_difference}</Paragraph>
-      <Paragraph>{repo.language}</Paragraph>
-      <Paragraph>{startIcon}  {repo.stargazers_count}</Paragraph>
-      
-    </Card.Content>
-    <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
-    </Card.Actions>
-  </Card>)
+  return (
+    <Card style={{margin:4}}>
+      <Card.Title title={repo.full_name} left={BookIcon} />
+      <Card.Content>
+        <Paragraph>{repo.description}</Paragraph>
+        <Paragraph>{time_difference}</Paragraph>
+        <Paragraph>{repo.language}</Paragraph>
+        <Paragraph>{starIcon} {repo.stargazers_count}</Paragraph>
+        
+      </Card.Content>
+      <Card.Actions>
+        <Button>Cancel</Button>
+        <Button>Ok</Button>
+      </Card.Actions>
+    </Card>
+  )
 };
 
 //    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
