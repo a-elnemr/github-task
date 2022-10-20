f9a84e1eff9eaed831d0b0e702e95b0fe9814bf5
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-20 15:43:00 +0200

    Prepared ReposRoute for changes

--- react-native-app/src/routes/ReposRoute.js
+++ react-native-app/src/routes/ReposRoute.js
@@ -1,18 +1,12 @@
+/*
+
 import { Text } from "react-native";
 import { useGetGithubByNameQuery } from "../services/github";
 
 import ReposList from "../components/ReposList";
 import repos from "../components/repos_example";
-
-const ReposRouteTest = () =>{
-    console.log(repos);
-    const toRender = <ReposList repos={repos}/>;
-    return toRender;
-
-}
-
 const ReposRoute = () => {
-    const { data, error, isLoading } = useGetGithubByNameQuery("abc")
+    const { data, error, isLoading } = useGetGithubByNameQuery()
     console.log(data, error, isLoading);
 
     const toRender = error ? (
@@ -26,4 +20,91 @@ const ReposRoute = () => {
     return toRender;
 };
 
-export default ReposRouteTest;
+export default ReposRoute;
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from "react";
+import { Text, View } from "react-native";
+import { useGetGithubByNameQuery } from "../services/github";
+import { Button, Paragraph, Dialog, Portal } from 'react-native-paper';
+import ReposList from "../components/ReposList";
+
+import { List, Divider } from 'react-native-paper';
+
+
+
+
+
+
+
+
+const ReposRoute = () => {
+    
+    const [visible, setVisible] = React.useState(false);
+
+    const showDialog = () => setVisible(true);
+  
+    const hideDialog = () => setVisible(false);
+
+    const [language, setLanguage] = React.useState("Hi");
+    
+
+
+    
+    const { data, error, isFetching } = useGetGithubByNameQuery();
+    console.log(data, error, isFetching);
+
+
+
+    const toRender = error ? (
+            <Text>Oh no, there was an error</Text>
+        ) : isFetching ? (
+            <Text>Loading...</Text>
+        ) : data ? (
+            <ReposList repos={data.items}/>
+        ) : null;
+
+    return(
+    <View>
+<View>
+        <Button onPress={showDialog}>Language: {language}</Button>
+        
+        
+        <Portal>
+          <Dialog visible={visible} onDismiss={hideDialog}>
+            
+        <List.Item
+                title="Python"
+                onPress={()=>{setLanguage(10); hideDialog();}}
+        />
+        <Divider />
+
+
+
+
+          </Dialog>
+        </Portal>
+</View>
+        {toRender}
+    </View>
+    )
+    ;
+};
+
+export default ReposRoute;
