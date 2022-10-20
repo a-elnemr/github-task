9a6bfc3bc6f73b2ba7849d9c5cfb7d981c70c37d
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-20 10:35:26 +0200

    Added the star icon

--- react-native-app/src/components/ReposList.js
+++ react-native-app/src/components/ReposList.js
@@ -3,6 +3,7 @@ import { Text } from "react-native";
 import { List } from 'react-native-paper';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import moment from 'moment';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 
@@ -21,6 +22,7 @@ const RepoCard = ({repo}) => {
   const diff = Date.now()-Date.parse(repo.updated_at);
   var duration = moment.duration(diff);
   const time_difference = "Updated "+duration.humanize() + " ago";
+  const startIcon = <Icon name="star-o" size={15} color="#00ee00" />;;
 
   return (<Card style={{margin:4}}>
     <Card.Title title={repo.full_name} left={LeftContent} />
@@ -28,16 +30,17 @@ const RepoCard = ({repo}) => {
       <Paragraph>{repo.description}</Paragraph>
       <Paragraph>{time_difference}</Paragraph>
       <Paragraph>{repo.language}</Paragraph>
-      <Paragraph>  {repo.stargazers_count}</Paragraph>
+      <Paragraph>{startIcon}  {repo.stargazers_count}</Paragraph>
       
     </Card.Content>
-    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
     <Card.Actions>
       <Button>Cancel</Button>
       <Button>Ok</Button>
     </Card.Actions>
   </Card>)
 };
+
+//    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
 
 
 const ReposList = ( {repos} ) =>
