16c1b619a9821591153c76c4f175de047ef24946
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-21 21:59:02 +0200

    Added milango to header

--- react-native-app/src/components/Header.js
+++ react-native-app/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+import { default as Feather } from "react-native-vector-icons/Feather";
+
+const milangoIcon = <Feather name="message-circle" size={30} color="#00ee00" />;
 
 const Header = () => {
-  return <></>;
+  return (
+    <View>
+      <Text variant="headlineMedium">{milangoIcon} milango</Text>
+    </View>
+  );
 };
 
 export default Header;
