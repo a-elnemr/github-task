9391334205aba79922b372352d9e22b09946e5e0
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-21 22:46:24 +0200

    Added a useless theme button

--- react-native-app/src/components/Header.js
+++ react-native-app/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, Button, IconButton } from "react-native-paper";
 import { default as Feather } from "react-native-vector-icons/Feather";
 import { default as FontAwesome } from "react-native-vector-icons/FontAwesome";
+import { default as Entypo } from "react-native-vector-icons/Entypo";
 import styles from "../../styles";
 import { Link } from "../../react-router";
 
@@ -10,6 +11,20 @@ const milangoIcon = <Feather name="message-circle" size={30} color="#00ee00" />;
 const searchIcon = <FontAwesome name="search" size={25} />;
 
 const Header = () => {
+  const [theme, setTheme] = React.useState("dark");
+  const toggleTheme = () => {
+    setTheme((_theme) => {
+      return _theme === "dark" ? "light" : "dark";
+    });
+  };
+
+  const themeElement =
+    theme === "dark" ? (
+      <Entypo name="light-down" size={25} onPress={toggleTheme} />
+    ) : (
+      <Entypo name="light-up" size={25} onPress={toggleTheme} />
+    );
+
   return (
     <View>
       <View>
@@ -20,9 +35,9 @@ const Header = () => {
         >
           {milangoIcon} milango
         </Text>
+        {themeElement}
         {searchIcon}
       </View>
-      <View></View>
 
       <View style={styles.nav}>
         <Link to="/">
