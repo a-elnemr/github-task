3c36b265f3ef06fba50866c7fc9e098940c58e68
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-21 22:08:30 +0200

    Added a search icon

--- react-native-app/src/components/Header.js
+++ react-native-app/src/components/Header.js
@@ -2,13 +2,22 @@ import React from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 import { default as Feather } from "react-native-vector-icons/Feather";
+import { default as FontAwesome } from "react-native-vector-icons/FontAwesome";
 
 const milangoIcon = <Feather name="message-circle" size={30} color="#00ee00" />;
+const searchIcon = <FontAwesome name="search" size={25} />;
 
 const Header = () => {
   return (
     <View>
-      <Text variant="headlineMedium">{milangoIcon} milango</Text>
+      <Text
+        variant="headlineMedium"
+        style={{ fontWeight: "bold", color: "#7700ff" }}
+        color="#aa00aa"
+      >
+        {milangoIcon} milango
+      </Text>
+      {searchIcon}
     </View>
   );
 };
