89260de7c0d00805d0ee65651ec5e28f68cc0690
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-22 02:01:29 +0200

    Adjusted 30% of Explore card

--- react-native-app/src/Cards/ExploreCard.js
+++ react-native-app/src/Cards/ExploreCard.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import {
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+  Text,
+} from "react-native-paper";
 import moment from "moment";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { default as FoundationIcon } from "react-native-vector-icons/Foundation";
+import { View } from "react-native";
+import colors from "../../colors";
 
 const ExploreCard = ({ repo }) => {
   const BookIcon = (props) => (
@@ -14,15 +23,44 @@ const ExploreCard = ({ repo }) => {
   const starIcon = <Icon name="star-o" size={15} color="#00ee00" />;
 
   return (
-    <Card style={{ marginVertical: 4 }}>
+    <Card style={{ marginVertical: 4, borderRadius: 15 }}>
       <Card.Title title={repo.full_name} left={BookIcon} />
       <Card.Content>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: colors.grey }}>Trending Repository</Text>
+
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ fontWeight: "bold" }}>{starIcon} Star </Text>
+
+            <View
+              style={{
+                padding: 7,
+                backgroundColor: "rgb(255,200,255)",
+                borderRadius: 15,
+              }}
+            >
+              <Text style={{ fontWeight: "bold" }}>
+                {repo.stargazers_count}
+              </Text>
+            </View>
+          </View>
+        </View>
         <Paragraph>{repo.description}</Paragraph>
         <Paragraph>{time_difference}</Paragraph>
         <Paragraph>{repo.language}</Paragraph>
-        <Paragraph>
-          {starIcon} {repo.stargazers_count}
-        </Paragraph>
       </Card.Content>
     </Card>
   );
