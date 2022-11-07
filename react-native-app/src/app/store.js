64c10120c61e0b4b5b6959c557329f2b4488f35f
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-11-07 21:39:27 +0200

    Totally removed the counter feature

--- react-native-app/src/app/store.js
+++ react-native-app/src/app/store.js
@@ -1,18 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
-import counterReducer from "../features/counter/counterSlice";
 import { githubApi } from "../services/github";
 
 /*
- Counter reducer is useless here
-
  Requests are managed with Redux using "Redux Toolkit Query" (RTK Query) which is a feature in RTK
 */
 
 export const store = configureStore({
   reducer: {
-    // counter: counterReducer,
     [githubApi.reducerPath]: githubApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
