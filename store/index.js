692aa686b8489a9332cd6e68cb1bbc6a06708848
@salem52 <mohamed7arkat@outlook.com>
2023-07-12 14:09:29 +0300

    .

--- store/index.js
+++ store/index.js
@@ -1,16 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterSlice } from './filterSlice';
-import { cartSlice } from './cartSlice';
-import { apiSlice } from './apiSlice';
 
 export const store = configureStore({
   reducer: {
     filters: filterSlice.reducer,
-    cart: cartSlice.reducer,
-    api: apiSlice.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
 });
