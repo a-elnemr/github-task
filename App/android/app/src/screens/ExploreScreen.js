f94fa7006bba1cc0b13fe6c4c6cd59fcaf590b41
mennabassam <mennabassam11@gmail.com>
2024-07-11 15:11:48 +0300

    adjusted Explore screen using modal picker component

--- App/android/app/src/screens/ExploreScreen.js
+++ App/android/app/src/screens/ExploreScreen.js
@@ -10,15 +10,21 @@ import {useDispatch, useSelector} from 'react-redux';
 import colors from '../config/color';
 import ScreenTitle from '../components/ScreenTitle';
 import {fetchRepos, setPickerValue} from '../redux/actions/Actions';
-import PickerComponent from '../components/PickerComponent';
 import ExploreCard from '../components/ExploreCard';
+import ModalPicker from '../components/ModalPicker';
 
 function ExploreScreen(props) {
   const dispatch = useDispatch();
-  const {allRepos, loading, error, viewedRepos, pickerValue} = useSelector(
+  const {loading, error, viewedRepos, pickerValue, repoLanguages} = useSelector(
     state => state.repos,
   );
   const darkMode = useSelector(state => state.theme.darkMode);
+  const pickerOptions = [
+    {label: 'Top 10', value: 10},
+    {label: 'Top 50', value: 50},
+    {label: 'Top 100', value: 100},
+  ];
+
   const handleValueChange = value => {
     dispatch(setPickerValue(value));
   };
@@ -33,19 +39,33 @@ function ExploreScreen(props) {
         darkMode ? [styles.container, styles.darkContainer] : styles.container
       }>
       <ScreenTitle title="Explore Popular" />
-      <PickerComponent
-        pickerValue={pickerValue}
-        handleValueChange={handleValueChange}
-      />
+      <View>
+        <View style={styles.pickerView}>
+          <ModalPicker
+            label="View Top:"
+            allValues={pickerOptions}
+            pickerValue={pickerValue}
+            handleValueChange={handleValueChange}
+          />
+        </View>
+      </View>
 
       {loading ? (
-        <ActivityIndicator size="large" color={colors.cyan} />
+        <ActivityIndicator
+          style={styles.loadingMargin}
+          size="large"
+          color={colors.cyan}
+        />
+      ) : error ? (
+        <Text style={styles.loadingMargin}>Error loading data: {error}</Text>
       ) : (
         <FlatList
           data={viewedRepos}
           style={styles.flatListStyle}
+          keyExtractor={(item, index) => item.id.toString()}
           renderItem={({item}) => (
             <ExploreCard
+              key={item.id.toString()}
               starCount={item.stargazers_count}
               RepoTitle={item.full_name}
               RepoDescription={item.description}
@@ -68,8 +88,15 @@ const styles = StyleSheet.create({
     backgroundColor: colors.darker,
   },
   flatListStyle: {
-    paddingHorizontal: 25,
+    paddingHorizontal: 20,
     marginVertical: 20,
+  },
+  loadingMargin: {
+    marginVertical: 'auto',
+  },
+  pickerView: {
+    width: '40%',
+    marginLeft: 20,
   },
 });
 
